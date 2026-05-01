8d59cc307be907922d923be0d23e4c524836a6ae
devfwilson <wflorian-2024392@kinal.edu.gt>
2026-05-01 16:16:42 -0600

    feat: Agregar ruta de reservaciones

--- src/app/router/AppRoutes.jsx
+++ src/app/router/AppRoutes.jsx
@@ -1,11 +1,17 @@
 import { Routes, Route } from "react-router-dom"
 import { DashboardPage } from "../layouts/DashboardPage.jsx"
-
+import { Reservations } from "../../features/reservations/components/Reservations.jsx";
 
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path="/dashboard" element={<DashboardPage />} >
+            <Route 
+                path="/dashboard" 
+                element={
+                    <DashboardPage />
+                } 
+            >
+                <Route path="reservations" element={<Reservations />} />
             </Route>
         </Routes>
     )
