939e06c88eedfcd2dece020c2f7fc492d16fd941
Angel Fernando Lucero León <alucero-2021211@kinal.edu.gt>
2026-05-01 21:49:29 -0600

    feat: se agrego crud de restaurantes

--- src/app/router/AppRoutes.jsx
+++ src/app/router/AppRoutes.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import { DashboardPage } from "../layouts/DashboardPage.jsx"
 import { Reservations } from "../../features/reservations/components/Reservations.jsx";
 import { Invoices } from "../../features/invoices/components/Invoices.jsx";
+import { Restaurants } from "../../features/restaurants/components/Restaurants.jsx";
 
 export const AppRoutes = () => {
     return (
@@ -14,6 +15,8 @@ export const AppRoutes = () => {
             >
                 <Route path="reservations" element={<Reservations />} />
                 <Route path="invoices" element={<Invoices />} />
+=======
+                <Route path="restaurants" element={<Restaurants />} />
             </Route>
         </Routes>
     )
